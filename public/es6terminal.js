269c3c7c8facee2435fb087ee5885c0ae90a33be
senritsu <teh.b4k4@gmail.com>
2016-02-20 09:17:23 +0100

    initial commit

---
+++ public/es6terminal.js
@@ -0,0 +1,91 @@
+'use strict';
+
+class Terminal {
+    constructor(host) {
+        this.element = host
+        this.interactive = false
+
+        const node = (tag, cssClass, parent) => {
+            const node = document.createElement(tag)
+            if (cssClass) node.classList.add(cssClass)
+            if (parent) parent.appendChild(node)
+            return node
+        }
+
+        this.background = node('div', 'terminal-background', host)
+        this.input = node('input', null, this.background)
+        this.scrollContainer = node('div', 'terminal-scroll', this.background)
+        this.outputArea = node('p', 'terminal-output-area', this.scrollContainer)
+        this.inputArea = node('p', 'terminal-input-area')
+        this.userPrompt = node('span', null, this.inputArea)
+        this.userInput = node('span', null, this.inputArea)
+        this.inputCaret = node('span', 'terminal-caret', this.inputArea)
+
+        this.input.setAttribute('type','text')
+        this.inputCaret.textContent = 'C'
+
+        for (var i = 0; i < 5; i++) {
+            this.write(`Test ${i}`)
+        }
+
+        this.background.addEventListener('click', (event) => { if(this.inputArea.parentNode) this.input.focus() })
+        this.input.addEventListener('input', (event) => this.userInput.textContent = this.input.value)
+        this.input.addEventListener('blur', (event) => this.inputCaret.classList.remove('blink'))
+        this.input.addEventListener('focus', (event) => this.inputCaret.classList.add('blink'))
+    }
+
+    write(text) {
+        const div = document.createElement('div')
+        div.textContent = text
+        this.outputArea.appendChild(div)
+    }
+
+    finishInput(write) {
+        this.scrollContainer.removeChild(this.inputArea)
+
+        if (write) {
+            this.write(this.userPrompt.textContent + this.input.value)
+        }
+
+        const userInput = this.input.value
+        this.input.value = ''
+        this.userInput.textContent = ''
+        this.userPrompt.textContent = ''
+
+        this.scrollToBottom()
+
+        return userInput
+    }
+
+    startInteractive() {
+        this.interactive = true
+        const loop = () => this.prompt().then(() => { if (this.interactive) loop() })
+        loop()
+    }
+
+    stopInteractive() {
+        this.finishInput()
+        this.interactive = false
+    }
+
+    scrollToBottom() {
+        this.background.scrollTop = this.scrollContainer.scrollHeight
+    }
+
+    prompt() {
+        this.scrollContainer.appendChild(this.inputArea)
+        this.userPrompt.textContent = '>>> '
+
+        this.scrollToBottom()
+
+        return new Promise((resolve, reject) => {
+            const listener = (event) => {
+                if (event.keyCode == 13) {
+                    this.input.removeEventListener('keydown', listener)
+                    resolve(this.finishInput())
+                }
+            }
+            this.input.addEventListener('keydown', listener)
+        })
+    }
+}
